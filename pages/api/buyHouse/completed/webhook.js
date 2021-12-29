f0c162309b3af8fa917e73b0085aee040d378536
daniel conteh <contehdaniel1995@gmail.com>
2021-12-29 01:48:02 +0000

    improve stripe webhook2

--- pages/api/buyHouse/completed/webhook.js
+++ pages/api/buyHouse/completed/webhook.js
@@ -5,10 +5,14 @@ const stripe = require('stripe')(process.env.STRIP_SERVER_SIDE_KEY);
 
 
 const creatBookingCheckOut = async session => {
-    const house = session.client_reference_id;
-    const user = (await User.findOne({ email: session.customer_email }))._id;
-    const price = session.payment_intent.amount / 100;
-    await Puchase.create({ house, user, price });
+    // const house = session.client_reference_id;
+    // const user = (await User.findOne({ email: session.customer_email }))._id;
+    // const price = session.payment_intent.amount / 100;
+    await Puchase.create({
+      house: '612a0bb674f1a82d902b789e',
+      user: '612a0bb674f1a82d902b7891',
+      price: 90000,
+    });
         
   
 }
