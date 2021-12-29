7556307c802da32bc041474066ad06f43c253277
daniel conteh <contehdaniel1995@gmail.com>
2021-12-29 11:50:33 +0000

    delete review data

--- BuyHouse/buyHouse.js
+++ BuyHouse/buyHouse.js
@@ -58,9 +58,10 @@ export const buyHouse =  catchAsync(async (req, res, next) => {
 
 
 
+// don't use catchAsync here 💥
 export const deleteHouse = async (req, res) => {
   const data = await Puchase.deleteOne({ _id: req.query.slug })
- return res.status(200).json({
+ return res.status(204).json({
   data
  });
  }
