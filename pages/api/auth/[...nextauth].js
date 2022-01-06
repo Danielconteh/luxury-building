03a13b8546524c9ccbdca294c039b34959917d1e
daniel conteh <contehdaniel1995@gmail.com>
2022-01-06 04:12:39 +0000

    auth modification1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -23,6 +23,21 @@ export default (req, res) =>
     ],
     database: process.env.CONNECTION_URI,
     secret: process.env.NEXTAUTH_SECRET_KEY,
+    session: {
+      // Use JSON Web Tokens for session instead of database sessions.
+      // This option can be used with or without a database for users/accounts.
+      // Note: `strategy` should be set to 'jwt' if no database is used.
+      strategy: 'jwt',
+
+      // Seconds - How long until an idle session expires and is no longer valid.
+      // maxAge: 30 * 24 * 60 * 60, // 30 days
+
+      // Seconds - Throttle how frequently to write to database to extend a session.
+      // Use it to limit write operations. Set to 0 to always update the database.
+      // Note: This option is ignored if using JSON Web Tokens
+      // updateAge: 24 * 60 * 60, // 24 hours
+    },
+
     pages: {
       signIn: '/signIn',
     },
@@ -36,8 +51,8 @@ export default (req, res) =>
         //  "token" is being send below to "session" callback...
         //  ...so we set "user" param of "token" to object from "authorize"...
         //  ...and return it...
-        user.ID = '1234'
-        user && (token.Id = user.ID);
+        // user.ID = '1234'
+        // user && (token.Id = user.ID);
         user && (token.user = user);
         return Promise.resolve(token); // ...here
       },
