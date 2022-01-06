208643395103d82c5e2633dcce86beff81368f18
daniel conteh <contehdaniel1995@gmail.com>
2022-01-06 02:59:34 +0000

    auth modification1

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,7 +19,6 @@ export default (req, res) =>
       Providers.GitHub({
         clientId: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        
       }),
     ],
     database: process.env.CONNECTION_URI,
@@ -31,13 +30,18 @@ export default (req, res) =>
       async redirect({ url, baseUrl }) {
         return baseUrl;
       },
-      // async session({ session, user, token }) {
-      //   // session.Id = user.ID;
-      //   return session;
-      // },
-      // async jwt({ token, user, account, profile, isNewUser }) {
 
-      //   return token;
+      async jwt({ token, user, account, profile, isNewUser }) {
+        console.log('JWT ==================');
+        if (user) {
+          user.ID = uuidv4();
+        }
+        return token;
+      },
+
+      // async session({ session, user, token }) {
+      //   session.Id = user.ID;
+      //   return session;
       // },
     },
   });
